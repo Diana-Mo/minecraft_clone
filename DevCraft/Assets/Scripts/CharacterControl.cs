b40654aee3808aae0768cbc8871f58842377edbf
Diana Ovod <ovod.diana@gmail.com>
2021-06-11 21:57:08 -0700

    character can move

--- DevCraft/Assets/Scripts/CharacterControl.cs
+++ DevCraft/Assets/Scripts/CharacterControl.cs
@@ -1,18 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//using UnityStandardAssets.CrossPlatformInput;
 
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Animator))]
 public class CharacterControl : MonoBehaviour
 {
+
+    [SerializeField] int moveSpeed;
+    [SerializeField] int jumpHeight;
+    private Rigidbody charRigidBody;
+    private Animator anim;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        charRigidBody = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 moveChar = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        transform.position += moveChar * Time.deltaTime * moveSpeed;
     }
 }
