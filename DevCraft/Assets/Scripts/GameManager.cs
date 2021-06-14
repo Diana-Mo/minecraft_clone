f8129b986c4574e24ed38455089c35b706e19e5d
Diana Ovod <ovod.diana@gmail.com>
2021-06-13 19:15:59 -0700

    isDoingAction set up
    to do:
    don't use buttons, instead using keyboard keys

--- DevCraft/Assets/Scripts/GameManager.cs
+++ DevCraft/Assets/Scripts/GameManager.cs
@@ -4,15 +4,77 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static GameManager instance = null;
+
+    private bool isJumping = false;
+    private bool isPunching = false;
+    private bool isBuilding = false;
+
+    public bool IsJumping
     {
-        
+        get
+        {
+            return isJumping;
+        }
+        set
+        {
+            isJumping = value;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public bool IsPunching
     {
-        
+        get
+        {
+            return isPunching;
+        }
+        set
+        {
+            isPunching = value;
+        }
+    }
+
+    public bool IsBuilding
+    {
+        get
+        {
+            return isBuilding;
+        }
+        set
+        {
+            isBuilding = value;
+        }
+    }
+
+    public void jumpBtnPressed()
+    {
+        IsJumping = true;
+    }
+
+    public void punchBtnPressed()
+    {
+        IsPunching = true;
+    }
+
+    public void buildBtnPressed()
+    {
+        IsBuilding = true;
+    }
+
+    static public GameManager Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        //DontDestroyOnLoad(gameObject); 
     }
 }
